d4a912693c1fea30740b85f0fdee1174fd530464
Sean Lai <sean.lai@mail.utoronto.ca>
2020-02-20 03:15:17 -0500

    Added sessionStorage

--- login.js
+++ login.js
@@ -1,7 +1,18 @@
 const home = document.querySelector('#home')
+const authentication = document.querySelector('#authentication')
 home.addEventListener('click', goHome)
+authentication.addEventListener('submit', login)
 
 function goHome(e) {
 	e.preventDefault()
 	window.location.href = 'index.html'
+}
+
+function login(e) {
+	e.preventDefault()
+	if (typeof(Storage) !== 'undefined') {
+		//TODO: Validate and setItem correctly
+		sessionStorage.setItem('profile', 'user')
+		window.location.href = 'index.html'
+	}
 }
