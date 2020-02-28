ddf3d1d473881b27447263dbe6b44ff5479287dc
Sean Lai <sean.lai@mail.utoronto.ca>
2020-02-28 14:37:52 -0500

    Deleted check for readyState

--- header.js
+++ header.js
@@ -1,10 +1,9 @@
 /* 
 General header redirect files
 */
-if(document.readyState === "complete"){
-	const home = document.querySelector('#home')
-	home.addEventListener('click', goHome)
-}
+const home = document.querySelector('#home')
+home.addEventListener('click', goHome)
+
 function goHome(e) {
 	e.preventDefault()
 	window.location.href = 'index.html'
