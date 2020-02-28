74d3a96f1ffbb7176c26b67d3f96c7a3f6b20b6b
Sean Lai <sean.lai@mail.utoronto.ca>
2020-02-28 00:26:19 -0500

    Fixed different declarations with same name

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const home = document.querySelector('#home')
 home.addEventListener('click', goHome)
 
-const profile = JSON.parse(sessionStorage.getItem('profile'))
+const profileData = JSON.parse(sessionStorage.getItem('profile'))
 
-if (profile != null) {
+if (profileData != null) {
 	const header = document.querySelector('#header')
 	header.firstElementChild.children[2].remove()
 	const profileContainer = document.createElement('li')
 	profileContainer.setAttribute('id', 'profileContainer')
 	const profileButton = document.createElement('button')
 	profileButton.setAttribute('id', 'profile')
-	const profileButtonText = document.createTextNode(profile)
+	const profileButtonText = document.createTextNode(profileData.username)
 	profileButton.appendChild(profileButtonText)
 	profileContainer.appendChild(profileButton)
 	header.firstElementChild.appendChild(profileContainer)
