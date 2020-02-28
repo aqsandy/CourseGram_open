2bf7f09fb809c4e4930d38e58e44097f7249db7a
wangmuy2 <aqsandy@live.com>
2020-02-28 15:20:00 -0500

    new function added in admin.js

--- admin.js
+++ admin.js
@@ -7,5 +7,59 @@ if(!storage.checkAdmin()){
     /* Push user info to the page  */
     const username_field = document.querySelector("#username");
     username_field.textContent = storage.getUsername();
-
+}
+function load_profile(){
+    const info = document.querySelector("#info");
+    if(info.querySelector("#embed")!=null){
+        //embeded page found in the div
+        info.removeChild(info.childNodes[0]);
+    }
+    const newelement = document.createElement("embed");
+    newelement.setAttribute(src,"./admin/profile.html");
+    newelement.setAttribute(id, "embed");
+    info.appendChild(newelement);
+}
+function load_program(){
+    const info = document.querySelector("#info");
+    if(info.querySelector("#embed")!=null){
+        //embeded page found in the div
+        info.removeChild(info.childNodes[0]);
+    }
+    const newelement = document.createElement("embed");
+    newelement.setAttribute(src,"./admin/program.html");
+    newelement.setAttribute(id, "embed");
+    info.appendChild(newelement);
+}
+function load_course(){
+    const info = document.querySelector("#info");
+    if(info.querySelector("#embed")!=null){
+        //embeded page found in the div
+        info.removeChild(info.childNodes[0]);
+    }
+    const newelement = document.createElement("embed");
+    newelement.setAttribute(src,"./admin/course.html");
+    newelement.setAttribute(id, "embed");
+    info.appendChild(newelement);
+}
+function load_requests(){
+    const info = document.querySelector("#info");
+    if(info.querySelector("#embed")!=null){
+        //embeded page found in the div
+        info.removeChild(info.childNodes[0]);
+    }
+    const newelement = document.createElement("embed");
+    newelement.setAttribute(src,"./admin/requests.html");
+    newelement.setAttribute(id, "embed");
+    info.appendChild(newelement);
+}
+function load_manage(){
+    const info = document.querySelector("#info");
+    if(info.querySelector("#embed")!=null){
+        //embeded page found in the div
+        info.removeChild(info.childNodes[0]);
+    }
+    const newelement = document.createElement("embed");
+    newelement.setAttribute(src,"./admin/manage.html");
+    newelement.setAttribute(id, "embed");
+    info.appendChild(newelement);
 }
