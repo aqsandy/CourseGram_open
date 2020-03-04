4c4119a7cf493c9007df3aae0957fef15495cb61
wangmuy2 <aqsandy@live.com>
2020-03-03 19:37:48 -0500

    Rough admin page finished. Embedded page has a problem to read the sessionstoage

--- admin.js
+++ admin.js
@@ -1,5 +1,4 @@
 const storage = new Storage()
-
 if(!storage.checkAdmin()){
     alert("Invalid access to the page!");
     window.location.href = './index.html'
@@ -10,56 +9,59 @@ if(!storage.checkAdmin()){
 }
 function load_profile(){
     const info = document.querySelector("#info");
-    if(info.querySelector("#embed")!=null){
+    console.log(info);
+    if(info.querySelector("#embed")!=='null'){
         //embeded page found in the div
         info.removeChild(info.childNodes[0]);
     }
     const newelement = document.createElement("embed");
-    newelement.setAttribute(src,"./admin/profile.html");
-    newelement.setAttribute(id, "embed");
+    newelement.setAttribute("src","./admin/profile.html");
+    newelement.setAttribute("id", "embed");
+    newelement.setAttribute("height","100%");
+    newelement.setAttribute("weight","100%");
     info.appendChild(newelement);
 }
 function load_program(){
     const info = document.querySelector("#info");
-    if(info.querySelector("#embed")!=null){
+    if(info.querySelector("#embed")!=='null'){
         //embeded page found in the div
         info.removeChild(info.childNodes[0]);
     }
     const newelement = document.createElement("embed");
-    newelement.setAttribute(src,"./admin/program.html");
-    newelement.setAttribute(id, "embed");
+    newelement.setAttribute("src","./admin/program.html");
+    newelement.setAttribute("id", "embed");
     info.appendChild(newelement);
 }
 function load_course(){
     const info = document.querySelector("#info");
-    if(info.querySelector("#embed")!=null){
+    if(info.querySelector("#embed")!=='null'){
         //embeded page found in the div
         info.removeChild(info.childNodes[0]);
     }
     const newelement = document.createElement("embed");
-    newelement.setAttribute(src,"./admin/course.html");
-    newelement.setAttribute(id, "embed");
+    newelement.setAttribute("src","./admin/course.html");
+    newelement.setAttribute("id", "embed");
     info.appendChild(newelement);
 }
 function load_requests(){
     const info = document.querySelector("#info");
-    if(info.querySelector("#embed")!=null){
+    if(info.querySelector("#embed")!=='null'){
         //embeded page found in the div
         info.removeChild(info.childNodes[0]);
     }
     const newelement = document.createElement("embed");
-    newelement.setAttribute(src,"./admin/requests.html");
-    newelement.setAttribute(id, "embed");
+    newelement.setAttribute("src","./admin/requests.html");
+    newelement.setAttribute("id", "embed");
     info.appendChild(newelement);
 }
 function load_manage(){
     const info = document.querySelector("#info");
-    if(info.querySelector("#embed")!=null){
+    if(info.querySelector("#embed")!=='null'){
         //embeded page found in the div
         info.removeChild(info.childNodes[0]);
     }
     const newelement = document.createElement("embed");
-    newelement.setAttribute(src,"./admin/manage.html");
-    newelement.setAttribute(id, "embed");
+    newelement.setAttribute("src","./admin/manage.html");
+    newelement.setAttribute("id", "embed");
     info.appendChild(newelement);
 }
