a0e9a93cceb1797a9aff0ba8138509c708f5391e
wangmuy2 <aqsandy@live.com>
2020-04-05 19:53:12 -0400

    Changes to admin

--- client/admin/total.js
+++ client/admin/total.js
@@ -101,64 +101,6 @@ const course_page = document.querySelector("#course");
 const profile_page = document.querySelector("#profile");
 const program_page = document.querySelector("#program");
 
-async function loading_courses(){
-    let courses = await get_courses()
-    const table = document.querySelector('tbody')
-    for(let i=0;i<courses.length;i++){
-        let campus = ''
-        switch(courses[i].courseCode.substr(7,8)){
-            case '1':
-                campus = "St.George"
-                break;
-            case '5':
-                campus = "Mississauga"
-        }
-        /* Buttons */
-        const delete_button = document.createElement('td')
-        delete_button.setAttribute('class','tb')
-        delete_button.setAttribute('id','opt')
-        const button1 = document.createElement('button')
-        button1.setAttribute('class','ui primary button')
-        button1.setAttribute('id','delete_course')
-        button1.innerText = 'Delete'
-        delete_button.appendChild(button1)
-        const edit_button = document.createElement('td')
-        edit_button.setAttribute('class','tb')
-        edit_button.setAttribute('id','opt')
-        const button2 = document.createElement('button')
-        button2.setAttribute('class','ui primary button')
-        button2.setAttribute('id','edit_course')
-        button2.innerText ='Edit'
-        edit_button.appendChild(button2)
-        const course = document.createElement('tr')
-        course.setAttribute('class','course')
-        course.setAttribute('id',courses[i].courseCode)
-        const course_code = document.createElement('td')
-        course_code.setAttribute('class','tb')
-        course_code.innerText=courses[i].courseCode
-        const course_name = document.createElement('td')
-        course_name.setAttribute('class','tb')
-        course_name.innerText=courses[i].data.courseTitle
-        const course_br = document.createElement('td')
-        course_br.setAttribute('class','tb')
-        course_br.innerText =courses[i].data.breadthCategories
-        const course_distribution = document.createElement('td')
-        course_distribution.setAttribute('class','tb')
-        course_distribution.innerText =courses[i].data.distributionCategories
-        const course_campus = document.createElement('td')
-        course_campus.setAttribute('class','tb')
-        course_campus.innerText=campus
-        course.appendChild(course_code)
-        course.appendChild(course_name)
-        course.appendChild(course_br)
-        course.appendChild(course_distribution)
-        course.appendChild(course_campus)
-        course.appendChild(delete_button)
-        course.appendChild(edit_button)
-        table.appendChild(course)
-    }
-}
-
 async function loading_programs(){
     let programs = await get_programs()
     const table = document.querySelector('tbody')
@@ -218,7 +160,6 @@ async function load_profiles(){
     let profiles = await get_profiles()
     console.log(profiles)
     const table = document.querySelector('table')
-
     for(let i=0;i<profiles.length;i++){
         const profile = document.createElement('tr')
         const id_num = document.createElement('td')
@@ -256,37 +197,49 @@ async function load_courses(){
     let courses = await get_courses()
     
     const table = document.querySelector('table')
-
+    
     for(let i=0;i<courses.length;i++){
         const course = courses[i].data
-        
+        let campus = ""
+        switch(courses[i].courseCode.substr(7,8)){
+            case '1':
+                campus = "St.George"
+                break;
+            case '5':
+                campus = "Mississauga"
+        }
         const course_row = document.createElement('tr')
-        const course_id = document.createElement('td')
         const course_name = document.createElement('td')
         const course_code = document.createElement('td')
         const delete_button = document.createElement('td')
+        const course_br = document.createElement('td')
         const del_but = document.createElement('button')
+        const course_distribution = document.createElement('td')
+        const course_campus = document.createElement('td')
 
-        course_id.setAttribute('class','tb')
         course_name.setAttribute('class','tb')
         course_code.setAttribute('class','tb')
         delete_button.setAttribute('class','tb')
+        course_br.setAttribute('class','tb')
+        course_distribution.setAttribute('class','tb')
+        course_campus.setAttribute('class','tb')
         delete_button.setAttribute('id','opt')
         del_but.setAttribute('class','ui primary button')
         del_but.setAttribute('id',courses[i].courseCode)
         del_but.innerHTML = "Delete"
         delete_button.appendChild(del_but)
         del_but.addEventListener("click", delete_course)
-        course_id.innerHTML = i;
         course_name.innerHTML = course.courseTitle
         course_code.innerHTML = courses[i].courseCode
-
-        
-        course_row.appendChild(course_id)
-        course_row.appendChild(course_name)
+        course_br.innerText =courses[i].data.breadthCategories
+        course_distribution.innerText =courses[i].data.distributionCategories
+        course_campus.innerText=campus
         course_row.appendChild(course_code)
+        course_row.appendChild(course_name)
+        course_row.appendChild(course_br)
+        course_row.appendChild(course_distribution)
+        course_row.appendChild(course_campus)
         course_row.appendChild(delete_button)
-        
         table.appendChild(course_row)
     }
 }
